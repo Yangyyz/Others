e6cef0b8f250fd73c500f65a9cc4da7767f60c8b
yangyizhou-github <yizhouyang@yeah.net>
2019-10-07 00:11:24 +0800

    /

--- ExhibitionSpider/spider.py
+++ ExhibitionSpider/spider.py
@@ -4,4 +4,3 @@ url='https://movie.douban.com/' #需要爬数据的网址
 page=requests.Session().get(url) 
 tree=html.fromstring(page.text) 
 result=tree.xpath('//td[@class="title"]//a/text()') #获取需要的数据
-print(result)
