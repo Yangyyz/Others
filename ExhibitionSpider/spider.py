6935ff34757e59a2a58d1c756623e1306f818ff7
yangyizhou-github <yizhouyang@yeah.net>
2019-10-07 11:49:45 +0800

    /

--- ExhibitionSpider/spider.py
+++ ExhibitionSpider/spider.py
@@ -1,6 +1,24 @@
 import requests
 from lxml import html
-url='https://movie.douban.com/' #需要爬数据的网址
+
+lists=[[] for i in range(10)]
+
+website="http://www.onezh.com"
+url='http://www.onezh.com/zhanhui/1_1_0_0_20190901/20190930/' 
 page=requests.Session().get(url) 
 tree=html.fromstring(page.text) 
-result=tree.xpath('//td[@class="title"]//a/text()') #获取需要的数据
+
+subpage=tree.xpath('//div[@class="row"]/a/@href') 
+
+page_sub=requests.Session().get(website+subpage[0])
+tree_sub=html.fromstring(page_sub.text)
+
+title=tree_sub.xpath('//div[@class="tuan-detail wrap"]/h1/text()')
+date=tree_sub.xpath('//dl[@class="tuan-info"]/dd/div/text()')
+pavilion=tree_sub.xpath('//div[@class="bao-key"]/a/span/text()')
+host=tree_sub.xpath('//dl[@class="tuan-info mp5"]/dd/text()')
+hostinfo=tree_sub.xpath('//div[@class="top_dealer_1"]/ul/li/text()')
+
+print(title,date,pavilion,host,hostinfo)
+
+
