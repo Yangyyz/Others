3546798639236000156224236233329027ca1f1f
yangyizhou-github <yizhouyang@yeah.net>
2019-10-09 20:09:36 +0800

    应用在循环中；处理了特殊情况下的一些BUG

--- ExhibitionSpider/spider.py
+++ ExhibitionSpider/spider.py
@@ -2,7 +2,7 @@ import requests
 import re
 from lxml import html
 
-lists=[[] for i in range(10)]
+lists=[["none" for j in range(10)] for i in range(50)]
 
 website="http://www.onezh.com"
 url='http://www.onezh.com/zhanhui/1_1_0_0_20190901/20190930/' 
@@ -11,55 +11,72 @@ tree=html.fromstring(page.text)
 
 subpage=tree.xpath('//div[@class="row"]/a/@href') 
 
-page_sub=requests.Session().get(website+subpage[0])
-tree_sub=html.fromstring(page_sub.text)
+loopControlVar=0
+for val in subpage:
 
-title=tree_sub.xpath('//div[@class="tuan-detail wrap"]/h1/text()')
-date=tree_sub.xpath('//dl[@class="tuan-info"]/dd/div/text()')
-pavilion=tree_sub.xpath('//div[@class="bao-key"]/a/span/text()')
-host=tree_sub.xpath('//dl[@class="tuan-info mp5"]/dd/text()')
-hostinfo=tree_sub.xpath('//div[@class="top_dealer_1"]/ul/li/text()')
+    page_sub=requests.Session().get(website+val)
+    tree_sub=html.fromstring(page_sub.text)
 
-# 标题
-pattern=re.compile(r'\s(\w+)')
-result=pattern.search(title[0])
-print(result.group(1))
+    title=tree_sub.xpath('//div[@class="tuan-detail wrap"]/h1/text()')
+    date=tree_sub.xpath('//dl[@class="tuan-info"]/dd/div/text()')
+    pavilion=tree_sub.xpath('//div[@class="bao-key"]/a/span/text()')
+    host=tree_sub.xpath('//dl[@class="tuan-info mp5"]/dd/text()')
+    hostinfo=tree_sub.xpath('//div[@class="top_dealer_1"]/ul/li/text()')
 
-#日期
-pattern=re.compile(r'(.+?)\xa0')
-result=pattern.search(date[0])
-#print(result.group(1))
+    # 标题
+    pattern=re.compile(r'\s(\w+)')
+    result=pattern.search(title[0])
+    #print(result.group(1))
 
-pattern=re.compile(r'(年|月)')
-result1=pattern.sub("/",result.group(1))
-#print(result1)
+    lists[loopControlVar][0]=result.group(1)
 
-pattern=re.compile(r'(日)')
-result2=pattern.sub("",result1)
-#print(result2)
+    #日期
+    pattern=re.compile(r'(.+?)\xa0')
+    result=pattern.search(date[0])
+    #print(result.group(1))
 
-pattern=re.compile(r'---')
-result3=pattern.sub("---2019/",result2)
-print(result3)
+    pattern=re.compile(r'(年|月)')
+    result1=pattern.sub("/",result.group(1))
+    #print(result1)
 
-#展馆
-print(pavilion[1])
+    pattern=re.compile(r'(日)')
+    result2=pattern.sub("",result1)
+    #print(result2)
 
-#主办单位
-pattern=re.compile(r'：(.+)')
-result=pattern.search(host[0])
-print(result.group(1))
+    pattern=re.compile(r'---')
+    result3=pattern.sub("---2019/",result2)
+    #print(result3)
 
-#承办单位
-pattern=re.compile(r'：(.+)')
-result=pattern.search(host[1])
-print(result.group(1))
+    lists[loopControlVar][1]=result3
 
-#官网
-pattern=re.compile(r'((www|http).+?)\'')
-result=pattern.search(str(hostinfo))
-print(result.group(1))
+    #展馆
+    #print(pavilion[1])
 
-print(title,date,pavilion,host,hostinfo)
+    if len(pavilion) > 1:
+        lists[loopControlVar][2]=pavilion[1]
 
 
+    #主办单位
+    pattern=re.compile(r'：(.+)')
+    result=pattern.search(host[0])
+    #print(result.group(1))
+    if result:
+        lists[loopControlVar][3]=result.group(1)
+
+    #承办单位
+    pattern=re.compile(r'：(.+)')
+    result=pattern.search(host[1])
+    #print(result.group(1))
+    if result:
+        lists[loopControlVar][4]=result.group(1)
+
+    #官网
+    pattern=re.compile(r'((www|http).+?)\'')
+    result=pattern.search(str(hostinfo))
+    #print(result.group(1))
+    if result:
+        lists[loopControlVar][5]=result.group(1)
+    
+    loopControlVar+=1
+
+print(lists)
