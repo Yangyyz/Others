7b39400f425d63cc1cd4e8fff724de380e8086a8
Yangyyz <34441609+Yangyyz@users.noreply.github.com>
2020-12-04 16:54:01 +0800

    加入自动填写届数

--- zhanhui.py
+++ zhanhui.py
@@ -4,15 +4,212 @@ from lxml import html
 import xlwings as xw
 import xlrd
 import xlwt
+import regex as re
 
 lists=[[" " for j in range(10)] for i in range(50)]
 
 website="http://www.onezh.com"
-url='http://www.onezh.com/zhanhui/1_21_0_0_20200901/20200930/' 
+url='http://www.onezh.com/zhanhui/1_1643_1683_0_20201001/20201031/' 
 page=requests.Session().get(url) 
 tree=html.fromstring(page.text) 
 
 subpage=tree.xpath('//div[@class="row"]/a/@href') 
+
+def number_translator(target):
+     
+    def word2number(s):
+        '''
+        可将[零-九]正确翻译为[0-9]
+
+        :param s: 大写数字
+        :return: 对应的整形数，如果不是数字返回-1
+        '''
+        if (s == u'零') or (s == '0'):
+            return 0
+        elif (s == u'一') or (s == '1') or (s == u'壹'):
+            return 1
+        elif (s == u'二') or (s == '2') or (s == u'贰') or (s == u'两'):
+            return 2
+        elif (s == u'三') or (s == '3') or (s == u'叁'):
+            return 3
+        elif (s == u'四') or (s == '4') or (s == u'肆'):
+            return 4
+        elif (s == u'五') or (s == '5') or (s == u'伍'):
+            return 5
+        elif (s == u'六') or (s == '6') or (s == u'陆'):
+            return 6
+        elif (s == u'七') or (s == '7') or (s == u'柒') or (s == u'天') or (s == u'日') or (s == u'末'):
+            return 7
+        elif (s == u'八') or (s == '8') or (s == u'捌'):
+            return 8
+        elif (s == u'九') or (s == '9') or (s == u'玖'):
+            return 9
+    #     elif (s == u'十') or (s == u'拾'):
+    #         return 10
+    #     elif (s == u'百') or (s == u'佰'):
+    #         return 100
+    #     elif (s == u'千') or (s == u'仟'):
+    #         return 1000
+    #     elif (s == u'万') or (s == u'萬'):
+    #         return 10000
+    #     elif (s == u'亿'):
+    #         return 100000000
+        else:
+            return -1
+        
+    def str2int(s):
+        '''
+        将字符数字转换为int
+        '''
+        try:
+            res = int(s)
+        except:
+            res = 0
+        return res
+    
+    pattern = re.compile(u"[一二两三四五六七八九123456789]万[一二两三四五六七八九123456789](?!(亿|千|百|十))")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"万")
+        s = list(s)
+        num = 0
+        if len(s) == 2:
+            num += word2number(s[0]) * 10000 + word2number(s[1]) * 1000
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+    
+    pattern = re.compile(u"[一二两三四五六七八九123456789]万[一二两三四五六七八九123456789](?!(千|百|十))")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"万")
+        s = list(s)
+        num = 0
+        if len(s) == 2:
+            num += word2number(s[0]) * 10000 + word2number(s[1]) * 1000
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+
+    pattern = re.compile(u"[一二两三四五六七八九123456789]千[一二两三四五六七八九123456789](?!(百|十))")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"千")
+        s = list(filter(None, s))
+        num = 0
+        if len(s) == 2:
+            num += word2number(s[0]) * 1000 + word2number(s[1]) * 100
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+
+    pattern = re.compile(u"[一二两三四五六七八九123456789]百[一二两三四五六七八九123456789](?!十)")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"百")
+        s = list(filter(None, s))
+        num = 0
+        if len(s) == 2:
+            num += word2number(s[0]) * 100 + word2number(s[1]) * 10
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+
+    pattern = re.compile(u"[零一二两三四五六七八九]")
+    match = pattern.finditer(target)
+    for m in match:
+        target = pattern.sub(str(word2number(m.group())), target, 1)
+#     print(target)
+
+    pattern = re.compile(u"(?<=(周|星期|天|日))[天|日|末]")
+    match = pattern.finditer(target)
+    for m in match:
+        target = pattern.sub(str(word2number(m.group())), target, 1)
+#     print(target)
+
+    pattern = re.compile(u"(?<!(周|星期))0?[0-9]?十[0-9]?")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"十")
+        num = 0
+        ten = str2int(s[0])
+        if ten == 0:
+            ten = 1
+        unit = str2int(s[1])
+        num = ten * 10 + unit
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+
+    pattern = re.compile(u"0?[1-9]百[0-9]?[0-9]?")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"百")
+        s = list(filter(None, s))
+        num = 0
+        if len(s) == 1:
+            hundred = int(s[0])
+            num += hundred * 100
+        elif len(s) == 2:
+            hundred = int(s[0])
+            num += hundred * 100
+            num += int(s[1])
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+
+    pattern = re.compile(u"0?[1-9]千[0-9]?[0-9]?[0-9]?")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"千")
+        s = list(filter(None, s))
+        num = 0
+        if len(s) == 1:
+            thousand = int(s[0])
+            num += thousand * 1000
+        elif len(s) == 2:
+            thousand = int(s[0])
+            num += thousand * 1000
+            num += int(s[1])
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+
+    pattern = re.compile(u"[0-9]+万[0-9]?[0-9]?[0-9]?[0-9]?")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"万")
+        s = list(filter(None, s))
+        num = 0
+        if len(s) == 1:
+            tenthousand = int(s[0])
+            num += tenthousand * 10000
+        elif len(s) == 2:
+            tenthousand = int(s[0])
+            num += tenthousand * 10000
+            num += int(s[1])
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+    
+    pattern = re.compile(u"[0-9]+亿[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?")
+    match = pattern.finditer(target)
+    for m in match:
+        group = m.group()
+        s = group.split(u"亿")
+        s = list(filter(None, s))
+        num = 0
+        if len(s) == 1:
+            tenthousand = int(s[0])
+            num += tenthousand * 100000000
+        elif len(s) == 2:
+            tenthousand = int(s[0])
+            num += tenthousand * 100000000
+            num += int(s[1])
+        target = pattern.sub(str(num), target, 1)
+#     print(target)
+
+    return target
 
 
 loopControlVar=0
@@ -33,6 +230,12 @@ for val in subpage:
     #print(result.group(1))
     if result:
         lists[loopControlVar][0]=result.group(1)
+
+    # 届数
+    pattern=re.compile(r'第(.+)届')
+    result=pattern.search(lists[loopControlVar][0])
+    if result:
+        lists[loopControlVar][9]=number_translator(result.group(1)) 
 
     #日期
     pattern=re.compile(r'(.+?)\xa0')
@@ -108,7 +311,7 @@ for val in subpage:
             lists[loopControlVar][6] = "农业林业渔业及农副产品"
             lists[loopControlVar][7] = "农业"
 
-    for key in {"林业","森林","树","园艺"}:
+    for key in {"林业","森林","树","园艺","竹产业"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "农业林业渔业及农副产品"
             lists[loopControlVar][7] = "林业"
@@ -118,17 +321,17 @@ for val in subpage:
             lists[loopControlVar][6] = "农业林业渔业及农副产品"
             lists[loopControlVar][7] = "渔业"
 
-    for key in {"畜牧业","牧","宠"}:
+    for key in {"畜牧业","牧","宠","奶业","猪"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "农业林业渔业及农副产品"
             lists[loopControlVar][7] = "畜牧业"
 
-    for key in {"农副产品加工","农副产品","农产品","乳业","乳制品"}:
+    for key in {"农副产品加工","农副产品","农产品","乳业","乳制品","粮油"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "农业林业渔业及农副产品"
             lists[loopControlVar][7] = "农副产品加工"
 
-    for key in {"食品制造","食品","烘培"}:
+    for key in {"食品制造","食品","烘培","烘焙"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "食品酒饮及服务"
             lists[loopControlVar][7] = "食品制造"
@@ -143,7 +346,7 @@ for val in subpage:
             lists[loopControlVar][6] = "食品酒饮及服务"
             lists[loopControlVar][7] = "餐饮服务"
 
-    for key in {"煤炭开采及加工","煤"}:
+    for key in {"煤炭开采及加工","煤","矿业"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "能源矿产"
             lists[loopControlVar][7] = "煤炭开采及加工"       
@@ -168,7 +371,7 @@ for val in subpage:
             lists[loopControlVar][6] = "能源矿产"
             lists[loopControlVar][7] = "非金属矿采选业"       
 
-    for key in {"其他矿产开采及加工"}:
+    for key in {"其他矿产开采及加工","钛业","冶金"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "能源矿产"
             lists[loopControlVar][7] = "其他矿产开采及加工"       
@@ -198,12 +401,12 @@ for val in subpage:
             lists[loopControlVar][6] = "工业科技"
             lists[loopControlVar][7] = "通用设备制造"
 
-    for key in {"电气机械和器材","电气","机械","军博会","科技","机床","电力","照明","紧固件","线缆","LED","3D打印","防锈","工业","光电","科技","五金","磨具"}:
+    for key in {"电气机械和器材","电气","机械","军博会","科技","机床","电力","照明","紧固件","线缆","LED","3D打印","防锈","工业","光电","科技","五金","磨具","热处理","变速机","燃料电池","泵阀"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "工业科技"
             lists[loopControlVar][7] = "电气机械和器材"
 
-    for key in {"电子器件","电子"}:
+    for key in {"电子器件","电子","半导体"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "工业科技"
             lists[loopControlVar][7] = "电子器件"
@@ -218,7 +421,7 @@ for val in subpage:
             lists[loopControlVar][6] = "工业科技"
             lists[loopControlVar][7] = "人工智能"
 
-    for key in {"环境保护及废弃资源综合利用","环保","资源利用","净水","新风","太阳能","排水","节能","新能源"}:
+    for key in {"环境保护及废弃资源综合利用","环保","资源利用","净水","新风","太阳能","排水","节能","新能源","风能"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "工业科技"
             lists[loopControlVar][7] = "环境保护及废弃资源综合利用"
@@ -233,12 +436,12 @@ for val in subpage:
             lists[loopControlVar][6] = "交通运输仓储和邮政"
             lists[loopControlVar][7] = "铁路交通运输"
 
-    for key in {"道路交通运输","汽车","卡车","车","汽摩"}:
+    for key in {"道路交通运输","汽车","卡车","车","汽摩","交通工程"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "交通运输仓储和邮政"
             lists[loopControlVar][7] = "道路交通运输"
 
-    for key in {"水上交通运输","船"}:
+    for key in {"水上交通运输","船","海事"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "交通运输仓储和邮政"
             lists[loopControlVar][7] = "水上交通运输"
@@ -328,7 +531,7 @@ for val in subpage:
             lists[loopControlVar][6] = "房屋建筑装修及经营服务"
             lists[loopControlVar][7] = "建筑安装"
 
-    for key in {"建筑装饰和其他建筑业","装修","家博会","涂料","供暖"}:
+    for key in {"建筑装饰和其他建筑业","装修","家博会","涂料","供暖","全屋整装"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "房屋建筑装修及经营服务"
             lists[loopControlVar][7] = "建筑装饰和其他建筑业"
@@ -338,7 +541,7 @@ for val in subpage:
             lists[loopControlVar][6] = "房屋建筑装修及经营服务"
             lists[loopControlVar][7] = "家装设计及家具"
 
-    for key in {"房地产"}:
+    for key in {"房地产","住宅"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "房屋建筑装修及经营服务"
             lists[loopControlVar][7] = "房地产"
@@ -363,12 +566,12 @@ for val in subpage:
             lists[loopControlVar][6] = "租赁和商务服务"
             lists[loopControlVar][7] = "人力资源"
 
-    for key in {"旅行及相关服务","旅游"}:
+    for key in {"旅行及相关服务","旅游","民宿","旅居"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "租赁和商务服务"
             lists[loopControlVar][7] = "旅行及相关服务"
 
-    for key in {"安全保护服务","安全","保护","安防","安保","应急"}:
+    for key in {"安全保护服务","安全","安全保护","安防","安保","应急"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "租赁和商务服务"
             lists[loopControlVar][7] = "安全保护服务"
@@ -378,7 +581,7 @@ for val in subpage:
             lists[loopControlVar][6] = "租赁和商务服务"
             lists[loopControlVar][7] = "电子商务"
 
-    for key in {"其他商务服务业","加盟","品牌","公益","环博会","丝绸之路","研讨会","洽谈会"}:
+    for key in {"其他商务服务业","加盟","品牌","公益","环博会","丝绸之路"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "租赁和商务服务"
             lists[loopControlVar][7] = "其他商务服务业"
@@ -408,7 +611,7 @@ for val in subpage:
             lists[loopControlVar][6] = "日用消费品及居民服务"
             lists[loopControlVar][7] = "化妆品卫生用品及美容美发服务"
 
-    for key in {"钟表眼镜","钟","表","眼镜"}:
+    for key in {"钟表眼镜","钟表","钟","眼镜"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "日用消费品及居民服务"
             lists[loopControlVar][7] = "钟表眼镜"
@@ -433,12 +636,12 @@ for val in subpage:
             lists[loopControlVar][6] = "日用消费品及居民服务"
             lists[loopControlVar][7] = "家政服务"
 
-    for key in {"婚庆设施及服务","婚"}:
+    for key in {"婚庆设施及服务","婚","喜庆用品"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "日用消费品及居民服务"
             lists[loopControlVar][7] = "婚庆设施及服务"
 
-    for key in {"其他产品及服务","消费品","商品","交易","特卖","展销","贸易"}:
+    for key in {"其他产品及服务","消费品","商品","交易","特卖","展销","贸易","婴童"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "日用消费品及居民服务"
             lists[loopControlVar][7] = "其他产品及服务"
@@ -463,22 +666,22 @@ for val in subpage:
             lists[loopControlVar][6] = "文化体育和娱乐"
             lists[loopControlVar][7] = "新闻和出版业"
 
-    for key in {"广播电视电影和影视制作","影视","电影","视频"}:
+    for key in {"广播电视电影和影视制作","影视","电影","视频","灯光音响","视听"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "文化体育和娱乐"
             lists[loopControlVar][7] = "广播电视电影和影视制作"
 
-    for key in {"文化艺术","艺术","文化","佛文化","石博会"}:
+    for key in {"文化艺术","艺术","佛文化","石博会","乐器","佛事"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "文化体育和娱乐"
             lists[loopControlVar][7] = "文化艺术"
 
-    for key in {"体育","运动","马术","健身"}:
+    for key in {"体育","运动","马术","健身","钓鱼","钓具"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "文化体育和娱乐"
             lists[loopControlVar][7] = "体育"
 
-    for key in {"娱乐"}:
+    for key in {"娱乐","动漫","游戏","酷狗蘑菇"}:
         if key in lists[loopControlVar][0]:
             lists[loopControlVar][6] = "文化体育和娱乐"
             lists[loopControlVar][7] = "娱乐"
@@ -498,6 +701,7 @@ for index in range(len(lists)):
     worksheet.write(index, 11, lists[index][7])
     worksheet.write(index, 27, "否")
     worksheet.write(index, 28, lists[index][8])
+    worksheet.write(index, 20, lists[index][9])
     
 
 workbook.save('xxxx.xls') 
